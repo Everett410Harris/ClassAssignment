38caa88bc52b47d650127d703fd7ed215497c531
Everett Harris <eve1harris2638@outlook.com>
2023-02-16 19:30:47 -0500

    Add project files.

---
+++ Software2KnowledgeCheck1/City.cs
@@ -0,0 +1,36 @@
+﻿using Software2KnowledgeCheck1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    internal class City : ConstructionServiceBase
+    {
+        public int ZipCode { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public int Population { get; set; }
+
+        public List<Building> Buildings { get; } = new List<Building>();
+
+        public void CreateApartment(Apartment apartment)
+        {
+            // Get materials
+            var ConstructionServiceBase = new ConstructionServiceBase();
+            var BuildingWasMade = ConstructionServiceBase.ConstructBuilding(apartment);
+
+            if (BuildingWasMade)
+            {
+                Buildings.Add(apartment);
+            }
+        }
+
+       
+     }
+}
+
+
+
---
+++ Software2KnowledgeCheck1/ConstructionServiceBase.cs
@@ -0,0 +1,38 @@
+﻿namespace Software2KnowledgeCheck1
+{
+    internal class ConstructionServiceBase : MaterialsRepo
+    {
+        private readonly MaterialsRepo materialRepo = new MaterialsRepo();
+        private readonly ZoningAndPermitRepo permitRepo = new ZoningAndPermitRepo();
+
+
+
+        public bool ConstructBuilding<T>(T Building) where T : Building
+        {
+            var materialsNeeded = materialRepo.GetMaterials();
+
+            if (permitRepo.GetPermit() && permitRepo.ZoningApproves())
+            {
+                foreach (var material in materialsNeeded)
+                {
+                    if (material == "concrete")
+                    {
+                        // start laying foundation
+                    }
+                    else if (material == "steel")
+                    {
+                        // start building structure
+                    }
+                    // etc etc...
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    
+    
+}
